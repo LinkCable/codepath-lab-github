4a97f6633c4a68adc5bda331e60b04126a1e9f25
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2016-02-17 19:17:40 -0500

    lab complete

---
+++ GithubDemo/GithubRepoCell.swift
@@ -0,0 +1,48 @@
+//
+//  GithubRepoCell.swift
+//  Gitzone
+//
+//  Created by Philippe Kimura-Thollander on 2/17/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class GithubRepoCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var starsImageView: UIImageView!
+    @IBOutlet weak var starsLabel: UILabel!
+    @IBOutlet weak var branchImageView: UIImageView!
+    @IBOutlet weak var branchLabel: UILabel!
+    
+    var repo: GithubRepo! {
+        didSet {
+            titleLabel.text = repo.name
+            authorLabel.text = "by \(repo.ownerHandle)"
+            descriptionLabel.text = repo.repoDescription
+            avatarImageView.setImageWithURL(NSURL(string: repo.ownerAvatarURL!)!)
+            starsLabel.text = String(repo.stars!)
+            branchLabel.text = String(repo.forks!)
+        }
+
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GithubDemo/RepoResultsViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  GithubDemo
+//
+//  Created by Nhan Nguyen on 5/12/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+import UIKit
+import MBProgressHUD
+
+// Main ViewController
+class RepoResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var searchBar: UISearchBar!
+    var searchSettings = GithubRepoSearchSettings()
+
+    var repos: [GithubRepo]!
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        // Initialize the UISearchBar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+
+        // Add SearchBar to the NavigationBar
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+
+        // Perform the first search when the view controller first loads
+        doSearch()
+    }
+
+    // Perform the search.
+    private func doSearch() {
+
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+
+        // Perform request to GitHub API to get the list of repositories
+        GithubRepo.fetchRepos(searchSettings, successCallback: { (newRepos) -> Void in
+
+            self.repos = newRepos
+            self.tableView.reloadData()
+            
+            print(self.repos)
+
+            MBProgressHUD.hideHUDForView(self.view, animated: true)
+            }, error: { (error) -> Void in
+                print(error)
+        })
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if repos != nil {
+            return repos.count
+        }
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("GithubRepoCell", forIndexPath: indexPath) as! GithubRepoCell
+        cell.repo = repos[indexPath.row]
+        return cell
+    }
+
+
+}
+
+// SearchBar methods
+extension RepoResultsViewController: UISearchBarDelegate {
+
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true;
+    }
+
+    func searchBarShouldEndEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true;
+    }
+
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchSettings.searchString = searchBar.text
+        searchBar.resignFirstResponder()
+        doSearch()
+    }
+}
